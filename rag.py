43105b41f1c8f1f4372732cae72a91ab58ba0292
Aayush Bhansali <aayushbhansali692@gmail.com>
2026-05-02 00:05:31 +0530

    Persist embedding database

--- rag.py
+++ rag.py
@@ -1,4 +1,6 @@
 import ollama
+import pickle
+import pathlib
 
 dataset = []
 
@@ -7,19 +9,13 @@ LANGUAGE_MODEL = 'hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF'
 
 VECTOR_DB = []
 
+pickled_file = "embeddings.bin"
+
 
 def add_chunk_to_database(chunk):
     embedding = ollama.embed(model=EMBEDDING_MODEL, input=chunk)['embeddings'][0]
     VECTOR_DB.append((chunk, embedding))
 
-with open("dataset.txt", "r", encoding="utf-8") as fs:
-    dataset = fs.readlines()
-    print(f"Loaded {len(dataset)} entries")
-
-
-for i, chunk in enumerate(dataset):
-    add_chunk_to_database(chunk)
-    print(f"Added chunk {i+1} / {len(dataset)} to the database")
 
 
 def cosine_similarity(a, b):
@@ -46,33 +42,59 @@ def retrieve(query, top_n=3):
     return similarities[:top_n]
 
 
-input_query = input("Ask me a question: ")
-retreived_knowledge = retrieve(input_query)
-
-print("Retrieved knowledge")
-for chunk, similarity in retreived_knowledge:
-    print(f"- similarity: {similarity:.2f} {chunk}")
-
-
-instruction_prompt = f"""
-You are a helpful chatbot. Use only the following pieces of information to answer the question, do not make up any new information.
-{'\n'.join(f"- {chunk}" for chunk, similarity in retreived_knowledge)}
-"""
+if pathlib.Path(pickled_file).exists():
+    print("Retreiving vector database from storage ...")
+    with open(pickled_file, 'rb') as fs:
+        VECTOR_DB = pickle.load(fs)
+else:
+    print("No stored vector database found, creating a new one ...")    
+    with open("dataset.txt", "r", encoding="utf-8") as fs:
+        dataset = fs.readlines()
+        print(f"Loaded {len(dataset)} entries")
 
 
-stream = ollama.chat(
-    model=LANGUAGE_MODEL,
-    messages=[
-        {'role': 'system', 'content': instruction_prompt},
-        {'role': 'user', 'content': input_query},
-    ],
-    stream=True
-)
+    for i, chunk in enumerate(dataset):
+        add_chunk_to_database(chunk)
+        print(f"Added chunk {i+1} / {len(dataset)} to the database")
+
+
+    print("Saving vector state ...")
+    with open(pickled_file, 'wb') as fs:
+        pickle.dump(VECTOR_DB, fs)
 
 
 # Print the response from the chatbot in realtime
-print('Chatbot response: ')
-for chunk in stream:
-    print(chunk['message']['content'], end='', flush=True)
+
+# Continue chat loop to avoid re-embedding everything
+while True:
+    input_query = input("\nAsk me a question: ")
 
 
+    if input_query == "exit":
+        break
+
+    retreived_knowledge = retrieve(input_query)
+
+    print("Retrieved knowledge")
+    for chunk, similarity in retreived_knowledge:
+        print(f"- similarity: {similarity:.2f} {chunk}")
+
+
+    instruction_prompt = f"""
+    You are a helpful chatbot. Use only the following pieces of information to answer the question, do not make up any new information.
+    {'\n'.join(f"- {chunk}" for chunk, similarity in retreived_knowledge)}
+    """
+
+
+    stream = ollama.chat(
+        model=LANGUAGE_MODEL,
+        messages=[
+            {'role': 'system', 'content': instruction_prompt},
+            {'role': 'user', 'content': input_query},
+        ],
+        stream=True
+    )
+
+    print('Chatbot response: ')
+    for chunk in stream:
+        print(chunk['message']['content'], end='', flush=True)
